31e37ad112c466af0165d755ae2fea30ca256fc1
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-22 23:28:07 -0700

    feature: Lots more options for example

--- examples/lovesense-test.rs
+++ examples/lovesense-test.rs
@@ -4,6 +4,20 @@ extern crate clap;
 use clap::{App};
 use lovesense::{LovesenseDevice};
 
+macro_rules! cmd {
+    (
+        $a: expr, $b:expr, $c:expr, $d:expr
+    ) => {
+        if $a.is_present($b) {
+            match $c {
+                Ok(s) => println!($d, s),
+                Err(s) => println!("{}", s)
+            }
+        }
+    }
+}
+
+
 fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("lovesense-test-cli.yml");
@@ -11,6 +25,12 @@ fn main() {
     let port = matches.value_of("port").unwrap();
     println!("Connecting to {}...", port);
     let mut d = LovesenseDevice::new(port);
+
+    cmd!(matches, "info", d.device_type(), "Device: {}");
+    cmd!(matches, "battery", d.battery_level(), "Battery Level: {}");
+    cmd!(matches, "status", d.device_status(), "Status: {}");
+    cmd!(matches, "power_off", d.power_off(), "Device powered off: {}");
+    cmd!(matches, "change_direction", d.change_rotation_direction(), "Rotation Direction Changed: {}");
 
     if matches.is_present("speed") {
         let speed = value_t!(matches, "speed", u8).unwrap_or(0);
@@ -20,18 +40,11 @@ fn main() {
         }
     }
 
-    if matches.is_present("info") {
-        match d.device_type() {
-            Ok(s) => println!("Device: {}", s),
+    if matches.is_present("rotate") {
+        let speed = value_t!(matches, "rotate", u8).unwrap_or(0);
+        match d.set_rotate(speed) {
+            Ok(_) => println!("Rotate set to {}", speed),
             Err(s) => println!("{}", s)
         }
     }
-
-    if matches.is_present("battery") {
-        match d.battery_level() {
-            Ok(s) => println!("Battery Level: {}%", s),
-            Err(s) => println!("{}", s)
-        }
-    }
-
 }
