eddbf408768570482b738059e8b9b26f889e5031
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-22 22:13:00 -0700

    chore: Trying to expand examples

--- examples/lovesense-test.rs
+++ examples/lovesense-test.rs
@@ -1,11 +1,23 @@
-extern crate liblovense;
+extern crate lovesense;
 #[macro_use]
 extern crate clap;
 use clap::{App};
+use lovesense::{LovesenseDevice};
 
 fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
-    let yaml = load_yaml!("lovense-test-cli.yml");
+    let yaml = load_yaml!("lovesense-test-cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
     let port = matches.value_of("port").unwrap();
+    println!("Connecting to {}...", port);
+    let mut d = LovesenseDevice::new(port);
+    let speed = value_t!(matches, "speed", u8).unwrap_or(0);
+    let _ = d.set_vibrate(speed);
+    if matches.is_present("info") {
+        let result = d.battery_level();
+        match result {
+            Ok(s) => println!("Device: {}", s),
+            Err(s) => println!("{}", s)
+        }
+    }
 }
